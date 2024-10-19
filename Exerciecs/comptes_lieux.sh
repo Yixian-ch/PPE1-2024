ac0201964858ae2d039192eb6b7e5851bccb9018
hacker <xingyu.chen@sorbonne-nouvelle.fr>
2024-10-19 19:07:54 +0200

    ajout des exercices 1 et 2 pour scripts bash

---
+++ Exerciecs/comptes_lieux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+read -p "choisir une annee, si vous tapez *, ça veut dire toutes les années: " annee
+read -p "choisir un mois, si vous tapez *, ça veut dire tous les mois: " mois
+read -p "choisir le nombre de lieux à afficher :" lieux
+path=$HOME/Desktop/Exercice1/ann/$annee/$annee"_"$mois
+grep Location $path*.ann | cut --f=3 | sort | uniq -c | sort -n | tail -$lieux 
